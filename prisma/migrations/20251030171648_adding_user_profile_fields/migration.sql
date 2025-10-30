edd7abec61c10f793e3be73fce8091474ad96cff
Joyce Adelusi <jadelusi11@gmail.com>
2025-10-30 18:17:42 +0100

    added user profile fields

---
+++ prisma/migrations/20251030171648_adding_user_profile_fields/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "public"."UserProfile" ADD COLUMN     "favoriteGenres" TEXT[],
+ADD COLUMN     "headline" TEXT,
+ADD COLUMN     "instruments" TEXT[],
+ADD COLUMN     "vocalFach" TEXT;
