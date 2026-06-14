4d36f428ffa0acfd74d59e4b5c5f179e261acc7a
Joyce Adelusi <jadelusi11@gmail.com>
2026-06-14 22:15:44 +0100

    fix url when undefined

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -7,7 +7,7 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient
 }
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! })
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
 const prisma = globalForPrisma.prisma || new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
